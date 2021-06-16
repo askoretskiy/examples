0ac8e1fbf787a342a2254f37b0c8b84cd30e0aef
Artem Skoretskiy <artem.skoretskiy@gmail.com>
2021-06-16 13:19:22 +0200

    Add Dijkstra algorithm

--- algorithms/pathfinder.py
+++ algorithms/pathfinder.py
@@ -2,6 +2,7 @@
 #   python3.9 -m algorithms.pathfinder
 import collections
 import heapq
+import math
 import sys
 from typing import Dict, List, Optional, Protocol, Tuple, TypeVar
 
@@ -27,6 +28,22 @@ class SimpleGraph(Graph):
         return self.edges[node]
 
 
+class WeightedGraph(Graph):
+    def cost(self, from_node: Location, to_node: Location) -> float:
+        ...
+
+
+class SimpleWeightedGraph(SimpleGraph, WeightedGraph):
+    weights: Dict[Tuple[Location, Location], float]
+
+    def __init__(self):
+        super().__init__()
+        self.weights = {}
+
+    def cost(self, from_node: Location, to_node: Location) -> float:
+        return self.weights.get((from_node, to_node), 1)
+
+
 class PriorityQueue:
     items: List[Tuple[float, T]]
 
@@ -36,7 +53,7 @@ class PriorityQueue:
     def __bool__(self):
         return bool(self.items)
 
-    def get(self):
+    def pop(self):
         _priority, item = heapq.heappop(self.items)
         return item
 
@@ -62,6 +79,35 @@ def breadth_first_search(
                 node_parent[child] = node
                 frontier.append(child)
 
+    return get_route(goal, node_parent)
+
+
+def dijkstra_search(
+    graph: WeightedGraph, start: Location, goal: Location
+) -> Optional[List[Location]]:
+    frontier = PriorityQueue()
+    frontier.put(0, start)
+    node_parent = {start: None}
+    cost_so_far = {start: 0}
+
+    while frontier:
+        node = frontier.pop()
+        if node == goal:
+            break
+
+        for child in graph.neighbors(node):
+            new_cost = graph.cost(node, child) + cost_so_far[node]
+            if new_cost < cost_so_far.get(child, math.inf):
+                cost_so_far[child] = new_cost
+                node_parent[child] = node
+                frontier.put(new_cost, child)
+
+    return get_route(goal, node_parent)
+
+
+def get_route(
+    goal: Location, node_parent: Dict[Location, Optional[Location]]
+) -> Optional[List[Location]]:
     if goal not in node_parent:
         return
 
@@ -75,9 +121,9 @@ def breadth_first_search(
 
 
 class TestPathfinder:
-    def test_breadth_first_search(self):
-        graph = SimpleGraph()
-        graph.edges = {
+    @pytest.fixture
+    def edges(self):
+        return {
             "A": ["B"],
             "B": ["C"],
             "C": ["B", "D", "F"],
@@ -85,10 +131,23 @@ class TestPathfinder:
             "E": ["F"],
             "F": [],
         }
+
+    def test_breadth_first_search(self, edges):
+        graph = SimpleGraph()
+        graph.edges = edges
         assert breadth_first_search(graph, "A", "F") == ["A", "B", "C", "F"]
         assert breadth_first_search(graph, "A", "E") == ["A", "B", "C", "D", "E"]
         assert breadth_first_search(graph, "F", "A") is None
 
+    def test_two(self, edges):
+        graph = SimpleWeightedGraph()
+        graph.edges = edges
+        assert dijkstra_search(graph, "A", "F") == ["A", "B", "C", "F"]
+        graph.weights = {("C", "F"): 3}
+        assert dijkstra_search(graph, "A", "F") == ["A", "B", "C", "F"]
+        graph.weights = {("C", "F"): 4}
+        assert dijkstra_search(graph, "A", "F") == ["A", "B", "C", "D", "E", "F"]
+
 
 if __name__ == "__main__":
-    pytest.main(sys.argv[:1])
+    pytest.main(sys.argv)
